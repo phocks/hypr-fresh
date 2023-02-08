00012dadabca2fdf11361d1358116d6ac1f4774f
Joshua Byrd <phocks@gmail.com>
2023-02-08 21:25:11 +1000

    Add badge delete Counter

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,6 +9,15 @@ export default function Home() {
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
         <SubmitForm />
+
+        <a href="https://fresh.deno.dev">
+          <img
+            width="197"
+            height="37"
+            src="https://fresh.deno.dev/fresh-badge.svg"
+            alt="Made with Fresh"
+          />
+        </a>
       </div>
     </>
   );
